6f562e4b211f6a95fb209d31984a928c6442e681
Mikołaj Mrózek <m.mik311@gmail.com>
2021-05-19 10:52:52 +0200

    added component to navigation

---
+++ src/components/LinkAnchor.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import '../styles/style.css'
+
+function LinkAnchor({ activeIdx, idx, colorScheme, text, setScreen }) {
+    const [hover, setHover] = useState(false)
+
+    return (
+        <div className='nav-button' style={{ backgroundColor: hover ? colorScheme.hover : null, padding: 0, margin: 0 }}>
+            <div className='nav-button' style={{ backgroundColor: (activeIdx === idx) ? colorScheme.backgroundCard : null }}
+                onClick={() => setScreen(idx)} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                <h4 style={{ color: (activeIdx === idx) ? colorScheme.primary : colorScheme.secondary }}>{text}</h4>
+            </div>
+
+        </div>
+    );
+}
+
+export default LinkAnchor
