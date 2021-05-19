db6a603ff5a659c8b7b8043b41ac62ba17e42900
Mikołaj Mrózek <m.mik311@gmail.com>
2021-05-19 10:52:33 +0200

    Create Contact.jsx

---
+++ src/screens/Contact.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/style.css'
+
+function Contact() {
+    return (<div />);
+}
+
+export default Contact;
