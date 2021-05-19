1f65bd7a619e4f5df382c772368583c7f73492a5
Mikołaj Mrózek <m.mik311@gmail.com>
2021-05-19 10:52:37 +0200

    Create About.jsx

---
+++ src/screens/About.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/style.css'
+
+function About() {
+    return (<div />);
+}
+
+export default About;
