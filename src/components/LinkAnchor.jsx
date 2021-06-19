9369e6711cfee888b3c2896998eda9bd9ac1defc
Mikołaj Mrózek <m.mik311@gmail.com>
2021-06-19 17:26:34 +0200

    Update LinkAnchor.jsx

--- src/components/LinkAnchor.jsx
+++ src/components/LinkAnchor.jsx
@@ -5,7 +5,7 @@ function LinkAnchor({ activeIdx, idx, colorScheme, text, setScreen }) {
     const [hover, setHover] = useState(false)
 
     return (
-        <div className='nav-button' style={{ backgroundColor: hover ? colorScheme.hover : null, padding: 0, margin: 0 }}>
+        <div className='nav-sheet' style={{ backgroundColor: hover ? colorScheme.hover : null, }}>
             <div className='nav-button' style={{ backgroundColor: (activeIdx === idx) ? colorScheme.backgroundCard : null }}
                 onClick={() => setScreen(idx)} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <h4 style={{ color: (activeIdx === idx) ? colorScheme.primary : colorScheme.secondary }}>{text}</h4>
